288ac9747ac171e33f83d8627fe668c02d28b5c4
Yair Vogel <yairvogel@github.com>
2024-04-11 09:02:13 +0200

    deserialize assets.json

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+string json = File.ReadAllText("obj/project.assets.json");
+var assets = JsonSerializer.Deserialize<Assets>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+Console.WriteLine(assets!.Version);
+foreach (var target in assets!.Targets)
+{
+    Console.WriteLine(target.Key);
+    foreach (var library in target.Value)
+    {
+        Console.WriteLine($"\t{library.Key}");
+        Console.WriteLine($"\t\t{library.Value.Type}");
+    }
+
+}
+
+public class Assets
+{
+    public int Version { get; set; }
+
+    public Targets Targets { get; set; } = new();
+}
+
+public class Targets : Dictionary<string, Target>
+{
+}
+
+public class Target : Dictionary<string, AssetLibrary>
+{
+}
+
+public class AssetLibrary
+{
+    public string Type { get; set; } = string.Empty;
+    
+    public Dictionary<string, string> Dependencies { get; set; } = new();
+}
