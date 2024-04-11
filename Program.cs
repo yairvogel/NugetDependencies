56b8c99547ed4b3e1ed9c131f10cc86671256b96
Yair Vogel <yairvogel@github.com>
2024-04-11 18:48:59 +0200

    extract version and create projectLibrary class as part of graph

--- Program.cs
+++ Program.cs
@@ -3,16 +3,12 @@
 string json = File.ReadAllText("obj/project.assets.json");
 var assets = JsonSerializer.Deserialize<Assets>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
-Console.WriteLine(assets!.Version);
-foreach (var target in assets!.Targets)
-{
-    Console.WriteLine(target.Key);
-    foreach (var library in target.Value)
-    {
-        Console.WriteLine($"\t{library.Key}");
-        Console.WriteLine($"\t\t{library.Value.Type}");
-    }
+var projectLibraries = assets!.Targets.Single().Value.ToProjectLibraries();
 
+foreach (var lib in projectLibraries) 
+{
+    Console.WriteLine(lib);
+    Console.WriteLine("[ " + string.Join(",", lib.Dependencies.Select(x => x)) + " ]");
 }
 
 public class Assets
@@ -28,6 +24,22 @@ public class Targets : Dictionary<string, Target>
 
 public class Target : Dictionary<string, AssetLibrary>
 {
+    public IEnumerable<ProjectLibrary> ToProjectLibraries()
+    {
+        return this.Select(ToProjectLibrary);
+    }
+
+    private ProjectLibrary ToProjectLibrary(KeyValuePair<string, AssetLibrary> entry)
+    {
+        if (entry.Key.Split("/") is not [string packageName, string version]) {
+            throw new ArgumentException($"expected one forward slash (/) in the project identifier. got {entry.Key}");
+        }
+
+        return new ProjectLibrary(
+                PackageName: packageName,
+                Version: version,
+                Dependencies: entry.Value.Dependencies.Select(kvp => new PackageReference(kvp.Key, kvp.Value)).ToArray());
+    }
 }
 
 public class AssetLibrary
@@ -36,3 +48,7 @@ public class AssetLibrary
     
     public Dictionary<string, string> Dependencies { get; set; } = new();
 }
+
+public record PackageReference(string PackageName, string Version);
+
+public record ProjectLibrary(string PackageName, string Version, PackageReference[] Dependencies);
